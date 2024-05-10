2eb1ae496839f7f976ba9669b89554faff135197
Alexey Davydov <aa.davydov09@yandex.ru>
2024-05-10 11:42:55 +0300

    Initial commit

---
+++ anfisa_for_friends/ice_cream/models.py
@@ -0,0 +1,85 @@
+from django.db import models
+
+from core.models import PublishedModel
+
+
+class Category(PublishedModel):
+    title = models.CharField('Название', max_length=256)
+    slug = models.SlugField('Слаг', max_length=64, unique=True)
+    output_order = models.PositiveSmallIntegerField(
+        verbose_name='Порядок отображения',
+        default=100
+        )
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'Категории'
+        #ordering = ('id', '-title',)    #либо тут, либо во view через .object.order_by(), но это работаает также как включение исключения
+
+    def __str__(self):
+        return self.title
+
+
+class Topping(PublishedModel):
+    title = models.CharField('Название', max_length=256)
+    slug = models.SlugField('Слаг', max_length=64, unique=True)
+
+    class Meta:
+        verbose_name = 'Топпинг'
+        verbose_name_plural = 'Топпинги'
+
+    def __str__(self):
+        return self.title
+
+
+class Wrapper(PublishedModel):
+    title = models.CharField(
+        'Название',
+        max_length=256,
+        help_text='Уникальное название обёртки, не более 256 символов'
+    )
+
+    class Meta:
+        verbose_name = 'Обертка'
+        verbose_name_plural = 'Обертки'
+
+    def __str__(self):
+        return self.title
+
+
+class IceCream(PublishedModel):
+    title = models.CharField('Название', max_length=256)
+    description = models.TextField('Описание')
+    wrapper = models.OneToOneField(
+        Wrapper,
+        on_delete=models.SET_NULL,
+        related_name='ice_cream',
+        null=True,
+        blank=True,
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name='ice_creams',
+        verbose_name='Категория'
+    )
+    toppings = models.ManyToManyField(Topping)
+    is_on_main = models.BooleanField('На главную', default=False)
+
+    price = models.DecimalField(
+        max_digits=5,
+        decimal_places=2
+    )
+
+    output_order = models.PositiveSmallIntegerField(
+        default=100,
+        verbose_name="Порядок отображения"
+    )
+
+    class Meta:
+        verbose_name = 'Мороженое'
+        verbose_name_plural = 'Мороженое'
+        ordering = ('output_order', 'title')
+
+    def __str__(self):
+        return self.title
